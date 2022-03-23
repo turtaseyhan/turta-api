8a4c66f8790c8ed3ae935a89e7b1d875ed278d3c
turtaseyhan <64765029+turtaseyhan@users.noreply.github.com>
2022-03-23 17:07:49 +0300

    Update mongo.js

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const { mongoPath } = require('../req/configDiscord.json')
 
 module.exports = async () => {
   await mongoose.connect(process.env.MONGO_PATH, {
