63ac7a66940fa22a608f6740f8781a5ce26057a1
turtaseyhan <64765029+turtaseyhan@users.noreply.github.com>
2022-03-23 17:05:10 +0300

    Create server.js

---
+++ app/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const config = require("../turt-djs-13/req/configDiscord.json");
+const mongoose = require("mongoose");
+const mongo = require("../turt-djs-13/mongodb/mongo.js");
+const ejs = require("ejs");
+const state_schema = require("../turt-djs-13/mongodb/schemas/state_schema.js");
+
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  await mongo().then(async (mongooose) => {
+    try {
+      // fetchs data from mongodb
+      await state_schema.find({}).then((data) => {
+        res.send(data[0].toJSON());
+      });
+    } finally {
+      mongooose.connection.close();
+    }
+  });
+});
+
+app.get("/arch", async (req, res) => {
+  await mongo().then(async (mongooose) => {
+    try {
+      // fetchs data from mongodb
+      await mongoose
+        .model("test", new mongoose.Schema({ test: String }))
+        .find({cpuArch})
+        .then((data) => {
+          res.send(data[0].toJSON());
+        });
+    } finally {
+      mongooose.connection.close();
+    }
+  });
+});
+app.listen(80, () => {
+  console.log("Server started on port 3000");
+});
