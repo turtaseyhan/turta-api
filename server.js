1cbbce2d1da125a777cc6922580582ccc58c0e50
turtaseyhan <64765029+turtaseyhan@users.noreply.github.com>
2022-03-23 17:18:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.listen(8080 || process.env.PORT, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on port 3000 or avaible Heroku Port");
 });
