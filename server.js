31a3a604ab1533cf977cc1021dc4ce99a6188846
turtaseyhan <64765029+turtaseyhan@users.noreply.github.com>
2022-09-21 00:26:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const mongo = require('./mongodb/mongo.js');
 const ejs = require("ejs");
+const hwid = require("hwid");
 const state_schema = require("./mongodb/schemas/state_schema.js");
 
 app.set("view engine", "ejs");
@@ -12,12 +13,13 @@ app.get("/", async (req, res) => {
     try {
       // fetchs data from mongodb
       await state_schema.find({}).then(data => {
-        res.send(data[0].toJSON());
+        res.send(data[0].toJSON(), '\n' + hwid());
       })
     } finally{
       mongooose.connection.close();
     }
   });
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
